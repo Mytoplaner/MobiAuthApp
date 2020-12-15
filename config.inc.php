f1845445a9a2b50968e59e220b50e1e429ff9a8c
Jordy <20169892@student.hhs.nl>
2020-12-16 00:50:18 +0100

    new

--- config.inc.php
+++ config.inc.php
@@ -1,12 +1,16 @@
 <?php
+if(PHP_SESSION_NONE){
+    session_start();
+}
+
 $db_host = 'localhost';
 $db_name = 'mobiauth';
 $db_password = '2O9n9f*n';
 $db_user = 'mobiauth';
 
-$con = mysqli_connect($db_host, $db_user, $db_password, $db_name);
+$conn = mysqli_connect($db_host, $db_user, $db_password, $db_name);
 if (mysqli_connect_errno() ) {
     // If there is an error with the connection, stop the script and display the error.
-    echo 'error, no database connection';
+    exit('Failed to connect to MySQL: ' . mysqli_connect_error());
 }
 ?>
--- login_process.php
+++ login_process.php
@@ -1,6 +1,19 @@
 <?php
-include 'config.inc.php';
+require_once 'config.inc.php';
 
-$email = $_POST['email'];
-$email = $_POST['password'];
+$email    = $_POST['email'];
+$password = $_POST['password'];
+
+$result = $conn->query("SELECT * FROM mphp4_users WHERE username = '$email' AND password = '$password'");
+
+if (mysqli_num_rows($result) == 1) {
+
+//  session_start();
+//  $_SESSION['em'] = $email;
+
+    header("Location:card.php");
+} else {
+    header("Location:index.php");
+    exit;
+}
 ?>
